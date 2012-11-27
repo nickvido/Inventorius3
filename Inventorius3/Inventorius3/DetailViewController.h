18862cae86b937c268d8af05b51a3c5e4d10ba53
Ken Harding <kenharding@Kens-MacBook-Pro.local>
2012-11-27 18:46:04 -0500

    Initial commit

---
+++ Inventorius3/Inventorius3/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Inventorius3
+//
+//  Created by Ken Harding on 11/27/12.
+//  Copyright (c) 2012 unit91. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ Inventorius3/Inventorius3/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  Inventorius3
+//
+//  Created by Ken Harding on 11/27/12.
+//  Copyright (c) 2012 unit91. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
